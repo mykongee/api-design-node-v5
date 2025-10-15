c97eab14337392532df275f54f4694bd3c781844
mykongee <mikehongde@gmail.com>
2025-10-14 21:33:28 -0400

    add additional local validation middleware

--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -22,3 +22,43 @@ export const validateBody = (schema: ZodSchema) => {
         }
     }
 }
+
+export const validateParams = (schema: ZodSchema) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            schema.parse(req.params);
+            next();
+        } catch (e) {
+            if (e instanceof ZodError) {
+                return res.status(400).json({
+                    error: 'Invalid route parameters',
+                    details: e.issues.map(err => ({
+                        field: err.path.join('.'),
+                        message: err.message,
+                    }))
+                });
+            }
+            next(e);
+        }
+    }
+}
+
+export const validateQuery = (schema: ZodSchema) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            schema.parse(req.query);
+            next();
+        } catch (e) {
+            if (e instanceof ZodError) {
+                return res.status(400).json({
+                    error: 'Invalid URL query parameters',
+                    details: e.issues.map(err => ({
+                        field: err.path.join('.'),
+                        message: err.message,
+                    }))
+                });
+            }
+            next(e);
+        }
+    }
+}
--- src/routes/habitRoutes.ts
+++ src/routes/habitRoutes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { validateBody } from "../middleware/validation.ts";
+import { validateBody, validateParams, validateQuery } from "../middleware/validation.ts";
 import { z } from 'zod';
 
 //@todo - placeholder
 const createHabitSchema = z.object({
     name: z.string(),
+});
+
+const completeParamsSchema = z.object({
+    id: z.string(),
+    // id: z.string().min(3).max(16),
 });
 
 const router = Router();
@@ -20,6 +25,14 @@ router.get('/:id', (req, res) => {
 router.post('/', validateBody(createHabitSchema), (req, res) => {
     res.json({ message: 'created habit' }).status(201);
 })
+
+router.post('/:id/complete',
+    validateParams(completeParamsSchema),
+    validateBody(createHabitSchema),
+    (req, res) => {
+        res.json({ message: 'completed habit' }).status(201);
+    }
+)
 
 router.delete('/:id', (req, res) => {
     res.json({ message: 'deleted habit' }).status(204);
